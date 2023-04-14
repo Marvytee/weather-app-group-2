4ad05aac6488f05ec249542651aa481aee97ebb8
EllaLawrie <lawrieella002@gmail.com>
2023-04-14 03:31:08 +0100

    quick fix

--- weather-app/src/assets/index.js
+++ weather-app/src/assets/index.js
@@ -1,5 +1,5 @@
 import mapIcon from './location.svg'
 import thunderIcon from './thunder.png'
-import thunderStormIcon from './thunderstorm.png'
+// import thunderStormIcon from './thunderstorm.png'
 
-export { mapIcon, thunderIcon, thunderStormIcon }
+export { mapIcon, thunderIcon }
